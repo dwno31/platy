df3b02145efaa9f485d215be6b98320133bc4cb8
대원노 <daewonnoh@daewonnoui-MacBook-Pro.local>
2017-04-27 18:01:12 +0900

    sada

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
 
-  devise_for :users, :controllers => { omniauth_callbacks: 'users/omniauth_callbacks' }
-  # , skip: ['sessions']
-  # as :user do
-  #  get 'users/sign_in' => redirect('/users/auth/kakao')
-  #  post 'users/sign_in', to: 'devise/sessions#create', as: :user_session
-  #  get 'users/sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session
-  # end
+  devise_for :users, :controllers => { omniauth_callbacks: 'users/omniauth_callbacks' }, skip: ['new_user_session']
+  as :user do
+   get 'users/sign_in' => redirect('/users/auth/kakao')
+  end
 
 
  root to:"front#index"
