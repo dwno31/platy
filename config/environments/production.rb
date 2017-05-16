7cf57d8fccfcf81b2cea8d40a1d3f32a25595aca
대원노 <daewonnoh@daewonnoui-MacBook-Pro.local>
2017-05-16 17:03:33 +0900

    produc

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,6 +53,9 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
   config.cache_store = :dalli_store,
       { :namespace => 'platy', :expires_in => 1.day, :compress => true }
+  config.public_file_server.headers = {
+      'Cache-Control' => 'public, max-age=172800'
+  }
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
